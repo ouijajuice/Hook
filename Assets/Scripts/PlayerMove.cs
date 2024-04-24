1dbd14c959387f1f95ac90fb36bace9d921f74e1
ouijajuice <eearls@ltu.edu>
2024-04-24 09:17:37 -0400

    holy stromboli

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class PlayerMove : MonoBehaviour
 {
 
@@ -17,6 +17,10 @@ public class PlayerMove : MonoBehaviour
     public int maxHealth = 5;
     public PlayerShooting playerShooting;
 
+    private bool hasKey = false;
+
+    private Vector3 move;
+
     // Update is called once per frame
     void Update()
     {
@@ -27,7 +31,7 @@ public class PlayerMove : MonoBehaviour
 
         bool jumping = Input.GetButtonDown("Jump");
 
-        Vector3 move = (transform.right * x + transform.forward * z).normalized;
+        move = (transform.right * x + transform.forward * z).normalized;
 
         if (jumping && grounded)
         {
@@ -38,23 +42,47 @@ public class PlayerMove : MonoBehaviour
         swayScript.counterMultiplier = swayScript.counterMultiplierInitial + Mathf.Abs(x) + Mathf.Abs(z) * swayMagnitude;
         swayScript.swayMagnitude = swayScript.swayMagnitudeInitial + swayMagnitudeChange;
 
-        player.position += move * speed * Time.deltaTime;
+        
     }
 
+    private void FixedUpdate()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(player.position, move, out hit, speed * Time.deltaTime))
+        {
+            // If the raycast hits something, adjust the movement vector
+            move = Vector3.ProjectOnPlane(move, hit.normal);
+        }
+        player.position += move * speed * Time.deltaTime;
+    }
     private void OnCollisionEnter(Collision collision)
     {
-        Debug.Log("COLLIDING");
+        //Debug.Log("COLLIDING");
         if (collision.collider.CompareTag("HarpoonPickup"))
         {
-            Debug.Log("Colliding");
+            //Debug.Log("Colliding");
             if (playerShooting.noHarpoon)
             {
-                Debug.Log("In noHapoon statement");
+                //Debug.Log("In noHapoon statement");
                 playerShooting.loaded = true;
                 playerShooting.noHarpoon = false;
 
             }
             collision.collider.gameObject.SetActive(false);
+        }
+        if (collision.collider.CompareTag("Key"))
+        {
+            hasKey = true;
+            collision.gameObject.SetActive(false);
+        }
+        if (collision.collider.CompareTag("KeyDoor") && hasKey == true)
+        {
+            collision.gameObject.SetActive(false);
+            hasKey = false;
+        }
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            SceneManager.LoadScene("Lose Screen");
         }
     }
 }
